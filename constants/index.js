d9958f2261599842a5fd91678f92cd409188cab3
Essoham SIBABI <richessoham@gmail.com>
2021-12-24 11:41:33 +0000

    signup commit

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+import icons from "./icons";
+import  Images from "./images";
+import theme, {COLORS, SIZES, FONTS} from "./theme";
+
+
+export { icons, Images, theme, COLORS, SIZES, FONTS }
---
+++ screens/index.js
@@ -0,0 +1,10 @@
+import Home from "./Home";
+import SignUp from "./SignUp";
+import Scan from "./Scan";
+
+
+export {
+    Home,
+    SignUp,
+    Scan
+}
