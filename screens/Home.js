8bea607619a7b833e682662be412e96aa3a6801d
Essoham SIBABI <richessoham@gmail.com>
2021-12-26 22:11:40 +0000

    home second commit

--- screens/Home.js
+++ screens/Home.js
@@ -101,7 +101,137 @@ const Home = () => {
     const [ features, setFeatures] = React.useState(featuresData)
     const [ specialPromos, setSpecialPromos] = React.useState(specialPromoData)
 
+    function renderHeader() {
+        return(
+             <View 
+                 style={{
+                     flexDirection: 'row',
+                     marginVertical: SIZES.padding * 2
+                 }}>
+                 <View
+                      style={{ flex: 1 }}>
+                      <Text style={{...FONTS.h1}}>Hello </Text>
+                      <Text style={{ ...FONTS.body2, color: COLORS.gray}}> Essoham SIBABI</Text>
+
+                      <View style={{ alignItems: 'center', justifyContent:'center', position: 'relative', top: -60, left:140}}>
+                          <TouchableOpacity
+                                    style={{
+                                        height: 40,
+                                        width: 40,
+                                        justifyContent: 'center',
+                                        alignItems: 'center',
+                                        backgroundColor: COLORS.lightGray
+                                    }}>
+                                    <Image 
+                                         source={icons.bell} 
+                                             style={{
+                                                 width: 20,
+                                                 height: 20,
+                                                 tintColor: COLORS.secondary
+                                             }}
+                                         />
+                                         <View
+                                             style={{
+                                                 position: 'absolute',
+                                                 top: -5,
+                                                 right: -5,
+                                                 height: 10,
+                                                 width: 10,
+                                                 backgroundColor: COLORS.red,
+                                                 borderRadius: 5
+                                             }}>
+
+                                         </View>
+                          </TouchableOpacity>
+                      </View>
+
+                 </View>
+
+             </View>
+        )
+    }
+
+    function renderBanner() {
+        return(
+            <View 
+                style={{
+                    height: 120,
+                    borderRadius: 20,
+                    
+                }}>
+                    <Image 
+                          source={Images.banner}
+                          resizeMode='cover'
+                          style={{
+                              width: "100%",
+                              height: "100%",
+                              borderRadius: 20
+                          }}
+                    />
+                </View>
+        )
+    }
+
+    function renderFeatures() {
+
+        const Header = () => (
+            <View style={{ marginBottom: SIZES.padding * 2 }}>
+                <Text style={{ ...FONTS.h3 }}>Features</Text>
+            </View>
+        )
+
+        const renderItem = ({ item }) => (
+            <TouchableOpacity
+                style={{ marginBottom: SIZES.padding * 2, width: 60, alignItems: 'center' }}
+                onPress={() => console.log(item.description)}
+            >
+                <View
+                    style={{
+                        height: 50,
+                        width: 50,
+                        marginBottom: 5,
+                        borderRadius: 20,
+                        backgroundColor: item.backgroundColor,
+                        alignItems: 'center',
+                        justifyContent: 'center'
+                    }}
+                >
+                    <Image
+                        source={item.icon}
+                        resizeMode="contain"
+                        style={{
+                            height: 20,
+                            width: 20,
+                            tintColor: item.color
+                        }}
+                    />
+                </View>
+                <Text style={{ textAlign: 'center', flexWrap: 'wrap', ...FONTS.body4 }}>{item.description}</Text>
+            </TouchableOpacity>
+        )
+
+        return (
+            <FlatList
+                ListHeaderComponent={Header}
+                data={features}
+                numColumns={4}
+                columnWrapperStyle={{ justifyContent: 'space-between' }}
+                keyExtractor={item => `${item.id}`}
+                renderItem={renderItem}
+                style={{ marginTop: SIZES.padding * 2 }}
+            />
+        )
+    }
+
     function renderPromos() {
+
+        const HeaderComponent = () => (
+            <View>
+                {renderHeader()}
+                {renderBanner()}
+                {renderFeatures()}
+            </View>
+        )
 
         const renderItem = ({item}) => (
             <TouchableOpacity
@@ -129,10 +259,23 @@ const Home = () => {
                                     />
                          </View>
 
+                         <View
+                          style={{
+                              padding: SIZES.padding,
+                              backgroundColor: COLORS.lightGray,
+                              borderBottomLeftRadius: 20,
+                              borderBottomRightRadius: 20
+                          }}>
+                          <Text style={{...FONTS.h4}} >{item.title} </Text>
+                          <Text style={{...FONTS.body4}} >{item.description} </Text>
+
+                         </View>
+
             </TouchableOpacity>
         )
         return(
             <FlatList
+                 ListHeaderComponent={HeaderComponent}
                  contentContainerStyle={{ paddingHorizontal: SIZES.padding * 3}}
                  numColumns={2}
                  columnWrapperStyle={{justifyContent: 'space-between'}}
